6eafe461caa1c98d5cadb42c42544ee05ae67e89
Jesse T Holzer <jesse.holzer@pnnl.gov>
2020-09-07 14:20:22 -0700

    transformer cod1 = 1, 3, or anything else

--- py/xfmr_utils.py
+++ py/xfmr_utils.py
@@ -1,10 +1,13 @@
 
 def compute_xfmr_position(r):
-    if r.cod1 == 0:
+    if not (r.cod1 in [1, 3]):
         position = 0
         oper_val = 0.0
         oper_val_realized = 0.0
         resid = 0.0
+        mid_val = 0.0
+        step_size = 0.0
+        max_position = 0.0
     else:
         mid_val = 0.5 * (r.rma1 + r.rmi1)
         step_size = (r.rma1 - r.rmi1) / (r.ntp1 - 1.0)
@@ -20,4 +23,4 @@ def compute_xfmr_position(r):
             position = -max_position
         oper_val_realized = mid_val + step_size * position
         resid = oper_val - oper_val_realized
-    return (position, oper_val, oper_val_realized, resid)
+    return (position, oper_val, oper_val_realized, resid, mid_val, step_size, max_position)
